04c007b3c55de444d77292fcd680accfae3e7e60
Adrian Bruinhout <adrian@webfella.com.au>
2013-04-28 14:49:31 +1000

    refactoring swipe and rotate into their own js files

--- src/js/templates/swipe.js
+++ src/js/templates/swipe.js
@@ -55,18 +55,4 @@
 		});
 	};
 
-	tabletAds.rotation = function() {
-		document.body.addEventListener('touchmove',function(event){ event.preventDefault(); },false);
-		$('.background').spritespin({
-      width:        640,
-      height:       437,
-      frames:       34,
-      resX:         21760,
-      resY:         437,
-      sense:        -1,
-      source:       'images/rotate/bike34x1_big.jpg',
-      enableCanvas: true
-		});
-	};
-
 }).call(this);
