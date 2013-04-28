966f69c6a1b02e73448fe13f0258141c88bbb2b2
Adrian Bruinhout <adrian@webfella.com.au>
2013-04-28 20:07:37 +1000

    tidying up js formatting

--- src/js/rotate.js
+++ src/js/rotate.js
@@ -10460,14 +10460,14 @@ if ( typeof define === "function" && define.amd && define.amd.jQuery ) {
 	tabletAds.rotation = function() {
 		document.body.addEventListener('touchmove',function(event){ event.preventDefault(); },false);
 		$('.background').spritespin({
-      width:        640,
-      height:       437,
-      frames:       34,
-      resX:         21760,
-      resY:         437,
-      sense:        -1,
-      source:       'images/rotate/bike34x1_big.jpg',
-      enableCanvas: true
+			width:        640,
+			height:       437,
+			frames:       34,
+			resX:         21760,
+			resY:         437,
+			sense:        -1,
+			source:       'images/rotate/bike34x1_big.jpg',
+			enableCanvas: true
 		});
 	};
 
